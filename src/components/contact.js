94d457f5ca9491493a90987fa40c57f2f8b8a390
github username <github email address>
2019-01-16 13:42:53 -0500

    add contact page paragraph

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,6 +13,7 @@ class Contact extends Component {
               alt="headshot"
               style={{height: '250px'}}
             />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Cell>
         <Cell col={6}>half page</Cell>
       </Grid>
