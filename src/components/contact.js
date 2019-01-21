bc8d0ed5132ae4e3afd1ad16a844536d86fdfd11
github username <github email address>
2019-01-21 15:44:45 -0500

    add right side contact page header

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
   render() {
@@ -15,7 +15,23 @@ class Contact extends Component {
             />
             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit anim id est laborum.</p>
         </Cell>
-        <Cell col={6}>half page</Cell>
+        <Cell col={6}>
+        <h2>Contact Me</h2>
+        <hr/>
+
+            <List>
+            <ListItem>
+              <ListItemContent icon="person">Bryan Cranston</ListItemContent>
+            </ListItem>
+            <ListItem>
+              <ListItemContent icon="person">Aaron Paul</ListItemContent>
+            </ListItem>
+            <ListItem>
+              <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+            </ListItem>
+          </List>
+
+        </Cell>
       </Grid>
     </div>
     )
