4ddebdc7c4839449c9ace74d76163d23f6ddf983
github username <github email address>
2019-01-22 12:36:10 -0500

    design left side of resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,36 @@
 import React, { Component, PropTypes } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 class Resume extends Component {
   render() {
     return(
-      <div><h1> Resume Page </h1></div>
+      <Grid>
+        <Cell col={4}>
+          <div styel={{textAlign: 'center'}}>
+            <img
+              src="https://i.imgur.com/jSu8Bh2.png"
+              alt="headshot"
+              style={{height: '200px'}}
+            />
+          </div>
+          <h2 style={{paddingTop: '2em'}}>Elaine Anderson</h2>
+          <h4 style={{color: 'grey'}}>Programmer</h4>
+          <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          <p>Full-stack web developer with a passion for pursuing creative projects through critical thinking.  With experience in Ruby on Rails, JavaScript, and React and a background in Journalism, I discovered web development through a fierce desire to learn the unknown. I bring strong skills in team-building plus proactive independent work that help companies move efficiently and positively toward their goals.</p>
+          <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          <h5>Location</h5>
+          <p>Boston, MA</p>
+          <h5>Phone</h5>
+          <p>(603) 707-0905</p>
+          <h5>Email</h5>
+          <p>eparie@bu.edu</p>
+          <h5>Blog</h5>
+          <p>https://elaineparie.github.io/</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+        </Cell>
+        <Cell className="resume-right-col" col={8}>Right Side</Cell>
+      </Grid>
     )
   }
 }
