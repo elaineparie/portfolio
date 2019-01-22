de837c6c5db938fb414ac634ea7c8d6de2b1c812
github username <github email address>
2019-01-22 13:22:01 -0500

    start designing right side resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Grid, Cell } from 'react-mdl';
+import Education from "./education"
 
 
 class Resume extends Component {
@@ -25,11 +26,23 @@ class Resume extends Component {
           <p>(603) 707-0905</p>
           <h5>Email</h5>
           <p>eparie@bu.edu</p>
+          <h5>LinkedIn</h5>
+          <p>https://www.linkedin.com/in/elaineparieanderson/</p>
           <h5>Blog</h5>
           <p>https://elaineparie.github.io/</p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
         </Cell>
-        <Cell className="resume-right-col" col={8}>Right Side</Cell>
+        <Cell className="resume-right-col" col={8}>
+          <h2>Education</h2>
+          <Education
+            startYear={2013}
+            endYear={2017}
+            schoolName="Boston University"
+            schoolMajor="Journalism"
+            schoolMinor="Philosophy"
+            schoolGpa={3.74}
+          />
+        </Cell>
       </Grid>
     )
   }
