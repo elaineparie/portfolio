1b49f5823df6036e4fb9f975d9004d1a06b84543
github username <github email address>
2019-01-28 14:07:57 -0500

    center social icons

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -5,7 +5,7 @@ import { Grid, Cell } from 'react-mdl';
 class LandingPage extends Component {
   render() {
     return(
-      <div style={{width: '100%', margin: 'auto' }}>
+      <div style={{width: '100%', margin: 'auto'}}>
       <Grid className='landing-grid'>
       <Cell col={12}>
       <img
