00b2dc887ff7fca1fe27f630d0dab62648af6a99
github username <github email address>
2019-01-29 16:19:51 -0500

    add blogpost component

---
+++ src/components/blogpost.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class BlogPost extends Component {
+  render() {
+    return(
+      <Grid>
+      <Cell col={4}>
+      </Cell>
+      <Cell col={8}>
+      </Cell>
+      </Grid>
+    )
+  }
+}
+
+export default BlogPost;
